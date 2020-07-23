13053adab26081df9518e05e379334e16e4b891d
Rory Hamilton <rhamilton790@yahoo.co.uk>
2020-07-23 18:46:00 +0100

    change to connected guilds logging

--- bot.py
+++ bot.py
@@ -23,13 +23,10 @@ client = discord.Client()
 @client.event
 async def on_ready():
     for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
 
 @client.event
 async def on_message(message):
